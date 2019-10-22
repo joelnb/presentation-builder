04328a5df0325060aacf2bcac27751ab4335f98a
Joel Noyce Barnham <joelnbarnham@gmail.com>
2019-10-22 10:09:25 +0100

    Allow running from another directory

--- prepare.sh
+++ prepare.sh
@@ -5,12 +5,14 @@ function openBrowser(){
     xdg-open "http://localhost:3000"
 }
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 INPUT_DIR="${1:-$(pwd)/input}"
 OUTPUT_DIR="${2:-$(pwd)/output}"
 
 mkdir -p "{OUTPUT_DIR}"
 
-docker build -t presentation-builder:local ./.builder/
+docker build -t presentation-builder:local "${DIR}/.builder/"
 
 openBrowser &
 
